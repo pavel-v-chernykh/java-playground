937b4d3ccbdaa5cc17faadb3d26a41ee177941f6
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-10-29 11:16:43 +0200

    Add result parser

---
+++ monadic-parser-combinators/src/main/java/com/example/playground/monadic/parser/combinators/Parsers.java
@@ -0,0 +1,16 @@
+package com.example.playground.monadic.parser.combinators;
+
+import com.example.playground.result.Result;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.stream.BaseStream;
+
+import static com.example.playground.monadic.parser.combinators.ParserResult.parserResult;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Parsers {
+    public static <T, E, I, S extends BaseStream<I, S>> Parser<T, E, I, S> result(T result) {
+        return input -> Result.result(parserResult(result, input));
+    }
+}
