d4554095d509253f060ad558ad6c246022f4c94c
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-10-30 10:11:08 +0200

    Add exact combinator

--- monadic-parser-combinators/src/main/java/com/example/playground/monadic/parser/combinators/Combinators.java
+++ monadic-parser-combinators/src/main/java/com/example/playground/monadic/parser/combinators/Combinators.java
@@ -8,6 +8,8 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.BaseStream;
 
+import static java.util.function.Predicate.isEqual;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Combinators {
     public static <T1, T2, E, I, S extends BaseStream<I, S>> Parser<T2, E, I, S> bind(Parser<T1, E, I, S> p1,
@@ -23,5 +25,9 @@ public final class Combinators {
     public static <T1, T2, E, I, S extends BaseStream<I, S>> Parser<Pair<T1, T2>, E, I, S> seq(Parser<T1, E, I, S> p1,
                                                                                                Parser<T2, E, I, S> p2) {
         return Combinators.bind(p1, t1 -> Combinators.bind(p2, t2 -> Parsers.result(Pair.pair(t1, t2))));
+    }
+
+    public static <T, E, S extends BaseStream<T, S>> Parser<T, E, T, S> exact(T item, E error) {
+        return Combinators.sat(isEqual(item), error);
     }
 }
