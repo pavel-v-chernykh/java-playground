57372ba181a683f36a20163ed8c44ac74b11eaec
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-11-14 22:40:57 +0200

    Add sat parser test

---
+++ monadic-parser-combinators/src/test/java/com/example/playground/monadic/parser/combinators/SatParserTest.java
@@ -0,0 +1,18 @@
+package com.example.playground.monadic.parser.combinators;
+
+import org.junit.Test;
+
+import static com.example.playground.monadic.parser.combinators.ParserResult.parserResultList;
+import static com.example.playground.monadic.parser.combinators.Parsers.sat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SatParserTest {
+    @Test
+    public void shouldAcceptCharSatisfyingThePredicate() {
+        final Parser parser = sat(s -> s.codePoints().allMatch(Character::isDigit));
+
+        assertThat(parser.parse("1world"), is(equalTo(parserResultList("1", "world"))));
+    }
+}
