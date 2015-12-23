5c4903b5167f1240f8a6058302253992623e9e2c
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-12-23 10:15:32 +0200

    Implement visitor for bool expression Add evaluation visitor

--- monadic-parser-combinators/src/test/java/com/example/playground/monadic/parser/combinators/ExpressionParserTest.java
+++ monadic-parser-combinators/src/test/java/com/example/playground/monadic/parser/combinators/ExpressionParserTest.java
@@ -6,12 +6,24 @@ import lombok.NoArgsConstructor;
 import lombok.Value;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.example.playground.monadic.parser.combinators.Parsers.*;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+interface ExpressionVisitor {
+    void visit(Identifier identifier);
+
+    void visit(Operator operator);
+
+    void visit(BoolExpression expression);
+}
+
 interface Expression {
+    void accept(ExpressionVisitor visitor);
 }
 
 @Value
@@ -21,6 +33,11 @@ class Identifier implements Expression {
     public static Identifier of(String id) {
         return new Identifier(id);
     }
+
+    @Override
+    public void accept(ExpressionVisitor visitor) {
+        visitor.visit(this);
+    }
 }
 
 @Value
@@ -29,6 +46,11 @@ class Operator implements Expression {
 
     public static Operator of(String operator) {
         return new Operator(operator);
+    }
+
+    @Override
+    public void accept(ExpressionVisitor visitor) {
+        visitor.visit(this);
     }
 }
 
@@ -40,6 +62,52 @@ class BoolExpression implements Expression {
 
     public static BoolExpression of(Expression left, Expression operator, Expression right) {
         return new BoolExpression(left, operator, right);
+    }
+
+    @Override
+    public void accept(ExpressionVisitor visitor) {
+        visitor.visit(this);
+    }
+}
+
+class ExpressionEvaluatingVisitor implements ExpressionVisitor {
+    private final Map<Identifier, Boolean> context = new HashMap<>();
+    private boolean evaluationResult;
+    private String operator;
+
+    public ExpressionEvaluatingVisitor(Map<Identifier, Boolean> context) {
+        this.context.putAll(context);
+    }
+
+    @Override
+    public void visit(Identifier identifier) {
+        evaluationResult = context.get(identifier);
+    }
+
+    @Override
+    public void visit(Operator operator) {
+        this.operator = operator.getOperator();
+    }
+
+    @Override
+    public void visit(BoolExpression expression) {
+        expression.getLeft().accept(this);
+        boolean left = evaluationResult;
+        expression.getRight().accept(this);
+        boolean right = evaluationResult;
+        expression.getOperator().accept(this);
+        switch (operator) {
+            case "and":
+                evaluationResult = left && right;
+                break;
+            case "or":
+                evaluationResult = left || right;
+                break;
+        }
+    }
+
+    public boolean getEvaluationResult() {
+        return evaluationResult;
     }
 }
 
@@ -79,7 +147,6 @@ final class ExpressionParsers {
 public class ExpressionParserTest {
     @Test
     public void parseBooleanExpressionWithTrailingParenthesisExpression() {
-        // E1 AND E2 AND (E3 OR E4)
         final BoolExpression parsedBoolExpression =
                 BoolExpression.of(
                         Identifier.of("e1"),
@@ -92,12 +159,19 @@ public class ExpressionParserTest {
                                         Operator.of("or"),
                                         Identifier.of("e4")
                                 )));
+        final HashMap<Identifier, Boolean> context = new HashMap<>();
+        context.put(Identifier.of("e1"), true);
+        context.put(Identifier.of("e2"), true);
+        context.put(Identifier.of("e3"), false);
+        context.put(Identifier.of("e4"), true);
+        final ExpressionEvaluatingVisitor visitor = new ExpressionEvaluatingVisitor(context);
+        visitor.visit(parsedBoolExpression);
+        assertThat(visitor.getEvaluationResult(), is(true));
         assertThat(ExpressionParsers.ROOT.parse("e1 and e2 and (e3 or e4)"), is(equalTo(Parser.result(parsedBoolExpression, ""))));
     }
 
     @Test
     public void parseBooleanExpressionWithParenthesisExpressionInTheMiddle() {
-        // E1 AND E2 AND (E3 OR E4)
         final BoolExpression parsedBoolExpression =
                 BoolExpression.of(
                         Identifier.of("e1"),
@@ -111,13 +185,20 @@ public class ExpressionParserTest {
                                 Operator.of("and"),
                                 Identifier.of("e4")
                         ));
+        final HashMap<Identifier, Boolean> context = new HashMap<>();
+        context.put(Identifier.of("e1"), true);
+        context.put(Identifier.of("e2"), false);
+        context.put(Identifier.of("e3"), false);
+        context.put(Identifier.of("e4"), true);
+        final ExpressionEvaluatingVisitor visitor = new ExpressionEvaluatingVisitor(context);
+        visitor.visit(parsedBoolExpression);
+        assertThat(visitor.getEvaluationResult(), is(false));
         assertThat(ExpressionParsers.ROOT.parse("e1 and (e2 or e3) and e4"), is(equalTo(Parser.result(parsedBoolExpression, ""))));
 
     }
 
     @Test
     public void parseBooleanExpressionWithLeadingParenthesisExpression() {
-        // E1 AND E2 AND (E3 OR E4)
         final BoolExpression parsedBoolExpression =
                 BoolExpression.of(
                         BoolExpression.of(
@@ -131,6 +212,14 @@ public class ExpressionParserTest {
                                 Operator.of("and"),
                                 Identifier.of("e4")
                         ));
+        final HashMap<Identifier, Boolean> context = new HashMap<>();
+        context.put(Identifier.of("e1"), true);
+        context.put(Identifier.of("e2"), false);
+        context.put(Identifier.of("e3"), true);
+        context.put(Identifier.of("e4"), true);
+        final ExpressionEvaluatingVisitor visitor = new ExpressionEvaluatingVisitor(context);
+        visitor.visit(parsedBoolExpression);
+        assertThat(visitor.getEvaluationResult(), is(true));
         assertThat(ExpressionParsers.ROOT.parse("(e1 or e2) and e3 and e4"), is(equalTo(Parser.result(parsedBoolExpression, ""))));
     }
 }
